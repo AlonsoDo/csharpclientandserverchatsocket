4c6f3bec62a79b966cdc18c4be18a8e3f06dcd21
Alonso Dominguez Escalona <alonso.caspi@gmail.com>
2020-02-21 11:45:47 +0100

    first commit

---
+++ SocketChatServer/SocketChatServer/Datos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections;
+using System.Diagnostics;
+
+namespace SocketChatServer
+{
+    class Datos
+    {
+        //public static Process proc = new Process();
+    }
+}
